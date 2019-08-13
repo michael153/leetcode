4be3b0d6892028f6abff1e0ec9c05e0b1e721c0b
Michael Wan <m.wan@berkeley.edu>
2019-08-13 16:35:57 -0700

    Optimized by using pq

--- 632/632.py
+++ 632/632.py
@@ -1,4 +1,5 @@
 import bisect
+import heapq
 
 class Solution(object):
     def smallestRange(self, nums):
@@ -8,9 +9,11 @@ class Solution(object):
         """
         K = len(nums)
         nums = sorted(nums, key = lambda l: l[0])
+        
         # Get rid of duplicates
         for i in range(len(nums)):
             nums[i] = sorted(list(set(nums[i])))
+
         if K == 1:
             return [nums[0][0], nums[0][0]]
         else:
@@ -18,12 +21,16 @@ class Solution(object):
             ranks = [(nums[i][ptr[i]], i) for i in range(K)]
             ranks = sorted(ranks, key = lambda l: l[0])
 
-            smallest = ranks[0][1]
-            largest = ranks[-1][1]
+            pq = [(nums[i][ptr[i]], i) for i in range(K)]
+            heapq.heapify(pq)
+
+            smallest = pq[0][1]
+            largest = heapq.nlargest(1, pq)[0][1]
 
             min_range = [nums[smallest][ptr[smallest]], nums[largest][ptr[largest]]]
+            done = False
 
-            while True:
+            while not done:
                 left, right = nums[smallest][ptr[smallest]], nums[largest][ptr[largest]]
                 mag = right - left
                 min_mag = min_range[1] - min_range[0]
@@ -35,20 +42,15 @@ class Solution(object):
 
                 ptr[smallest] += 1
                 if ptr[smallest] == len(nums[smallest]):
+                    done = True
                     break
 
-                ranks = ranks[1:]
                 payload = (nums[smallest][ptr[smallest]], smallest)
-                ranks.insert(bisect.bisect_left(ranks, payload), payload)
+                heapq.heappushpop(pq, payload)
 
-                smallest = ranks[0][1]
-                largest = ranks[-1][1]
+                if payload[0] >= nums[largest][ptr[largest]]:
+                    largest = smallest
+
+                smallest = pq[0][1]
 
         return min_range
-
-
-a = Solution()
-nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
-ans = a.smallestRange(nums)
-
-print(ans)
